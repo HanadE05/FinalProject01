b18d34b4310181a181665dcb6dba8e19d11aea07
Hanad Eid <hanad.eid@outlook.com>
2024-10-29 19:55:36 +0000

    Initial Commit

---
+++ Swift TalkTests/Swift_TalkTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TalkTests.swift
+//  Swift TalkTests
+//
+//  Created by Ibrahim Eid on 29/10/2024.
+//
+
+import Testing
+@testable import Swift_Talk
+
+struct Swift_TalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
